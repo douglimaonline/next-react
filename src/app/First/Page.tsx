11cd4e4b7685030a43b09f1696962e37af6aeeca
douglas <douglimaonline@gmail.com>
2024-07-13 15:50:09 -0300

    first commit

---
+++ src/app/First/Page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Inicio () {
+    return (
+        <div>
+            <h1>Masterclass Next</h1>
+
+            <nav>
+                <link href="/first">Primeiro Componente</link>
+            </nav>
+        </div>
+    )
+}
